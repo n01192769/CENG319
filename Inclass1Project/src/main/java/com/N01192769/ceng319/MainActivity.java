0e9586f3c21587f64e35ed52904907b7bdcc51e5
n01192769 <n01192769@humbermail.ca>
2019-10-03 00:49:16 -0400

    splash screen

--- Inclass1Project/src/main/java/com/N01192769/ceng319/MainActivity.java
+++ Inclass1Project/src/main/java/com/N01192769/ceng319/MainActivity.java
@@ -2,43 +2,46 @@ package com.N01192769.ceng319;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
-
-import java.util.jar.Attributes;
+import android.content.Intent;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+private void displayData(){
+    setTheme(R.style.SplashScreenTheme);
 
-    public void stuff(){
-        Toast.makeText(getApplicationContext(), "Hello, this is my first Android APP", Toast.LENGTH_LONG).show();
+}
 
 
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                displayData();
+            }
+        },8000);
+
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
         Switch switch1 = findViewById(R.id.switch1);
         switch1.setOnClickListener ( new View.OnClickListener() {
                                          @Override
                                          public void onClick(View view) {
-                                                stuff();
-                                         }
-                                      });
-
-
-        //public void
-//        switch1.setChecked(true);
-
-
-
+                                             Toast.makeText(getApplicationContext(),
+                                                     "Hello, this is my first Android APP",
+                                                     Toast.LENGTH_LONG).show(); }});
 
         Button but = findViewById(R.id.button);
         but.setOnClickListener(new View.OnClickListener() {
@@ -46,16 +49,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Hello, this is my first Android APP", Toast.LENGTH_LONG).show();
             }
         });
-
-
         Button button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Should not click", Toast.LENGTH_LONG).show();
-
-            }
+                Toast.makeText(getApplicationContext(), "Should not click", Toast.LENGTH_LONG).show(); }
         });
+
+
 
     }}
 
